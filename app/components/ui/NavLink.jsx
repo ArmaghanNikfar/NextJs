344d620a47f7b15643393d9e60d470b900ce6f81
Armaghan Nikfar <armaghannikfar@gmail.com>
2024-04-12 18:13:31 +0330

    changes

---
+++ app/components/ui/NavLink.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+const NavLink = ({ href, ...rest }) => {
+    const pathName = usePathname()
+    const isActive = href === pathName
+    console.log(href === pathName);
+
+    return(
+        <Link className={isActive? 'bg-gray' : ''} href={href} {...rest} />
+    )
+}
+export default NavLink;
